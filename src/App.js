66f29f2203dda82ced71555b8055e3557057224e
manas-shinde <manas.a.shinde11@gmail.com>
2023-06-18 12:03:43 +0530

    Added toast msg for user.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import http from "./services/httpService";
 import config from "./config.json";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
   state = {
@@ -19,10 +21,12 @@ class App extends Component {
     let { data: post } = await http.post(config.apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
+    toast.success("New Post added.");
   };
 
   handleUpdate = async (post) => {
     let obj = { title: "UPDATED" };
+    toast("Updating the post in database.");
 
     let { data: updatedPost } = await http.patch(
       config.apiEndpoint + "/" + post["id"],
@@ -34,6 +38,7 @@ class App extends Component {
     let index = posts.indexOf(post);
     posts[index] = { ...updatedPost };
     this.setState({ posts });
+    toast.success(`Post Number : ${post.id} is updated.`);
   };
 
   handleDelete = async (post) => {
@@ -49,7 +54,7 @@ class App extends Component {
       await http.delete(config.apiEndpoint + "/" + post["id"]);
     } catch (e) {
       if (e.response && e.response.status === 404) {
-        alert("This post has already been deleted.");
+        toast.error("This post has already been deleted.");
       }
       this.setState({ posts: OriginalPosts });
     }
@@ -58,6 +63,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
